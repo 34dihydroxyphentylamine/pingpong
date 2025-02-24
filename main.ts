efe47d2ddba7c5be1504896fe0ae2a972f51f03d
34dihydroxyphentylamine <102799272+34dihydroxyphentylamine@users.noreply.github.com>
2025-02-24 18:12:38 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    led.unplot(Diode, 4)
+    Diode += -1
+    led.plot(Diode, 4)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    led.unplot(Diode, 4)
+    Diode += 1
+    led.plot(Diode, 4)
+})
+let Diode = 0
+Diode = 1
+led.plot(Diode, 4)
+basic.forever(function () {
+	
+})
